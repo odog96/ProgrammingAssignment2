bd39026be173b8cec44f17b75128fe5a281293c3
odog96 <41704658+odog96@users.noreply.github.com>
2018-10-07 15:26:10 -0500

    Updated cache matrix.
    Added ability to calculated inverse matrix. Also, added ability to check if given input matrix has been solved for by checking cache.

--- cachematrix.R
+++ cachematrix.R
@@ -1,32 +1,66 @@
-## Set of functions to invert an input matrix. Checks memory first
- 
-## makeCacheMatix- 4 functions-1.sets matrix . 2. gets matrix 
-## 3. sets inverse 4. gets inverse. Last - all funcitons go in list
+##################################################################
+##  Oliver Zarate,  October 10, 2018 
+##  Coursera R Programming Week 3, programning assignment 2
+##  makecachematrix and cachesolve functions - lexical scoping
+##
+##################################################################
+##  The set of functions below makecachematrix and cacheSolve work 
+##  in conjuction to provide the inverse of an input matrix, but
+##  the functions will first check to see if given input has already
+##  be solved for, and then simply pull from cache instead of
+##  calculating. 
+#################################################################
+## makecachematrix -function created to support CacheSolve function 
+## details shown in comments below. Requires input matrix and assignment
+## to object. Then can use cachematrix funtion afterwards.
 
-makeCacheMatrix <- function(x = matrix()) {
-    m <- NULL
-    set <- function(y) {
-        x <<- y
-        m <<- NULL
-    }
-    get <- function() x
-    setsolve <- function(solve) m <<- solve
-    getsolve <- function() m
+makecachematrix <- function(x = matrix()) {
+    ## 1. Resets the "inv.matrix" solution.
+    inv.matrix <- NULL
+    set <- function(y) { 
+        x <<- y             # assigns input matrix to x
+        inv.matrix <<- NULL # 1. Resets the "inv.matrix" solution
+    }                       # & assigns to parent environment.
+    get <- function() x     # function to return input matrix
+    ## function to assign solution in parent environment 
+    setsolve <- function(solve) inv.matrix <<- solve
+    ## funtion to get solution 
+    getsolve <- function() inv.matrix
+    ## creates a list object of funtion just created
     list(set = set, get = get,
          setsolve = setsolve,
          getsolve = getsolve)
 }
-## CacheSolve - 1. checks if inverse is in memory.if so returns from cache, 
-#  if so, will get from cache, if not will solve and place into cache.
+## function below is manages calculation method of input matrix 
+## Input argement 'x' must be a list created by makecache matrix 
+## function
 
+## function first checks if inv.matrix exists, which is why earlier
+## funtion assigned to parent argument. If it does exist (i.e. not null)
+## then it simply returns from inv.matrix variable
 cacheSolve <- function(x, ...) {
-    m <- x$getsolve()
-    if(!is.null(m)) {
+    inv.matrix <- x$getsolve()
+    if(!is.null(inv.matrix)) {
         message("getting cached data")
-        return(m)
+        return(inv.matrix)
     }
+    ## if inv.matrix is null, then uses get funtion to get input 
+    ## matrix & assigns to 'data'. Then uses solve to calculate
+    ## inverse matrix, then uses setsolve function to set solution
     data <- x$get()
-    m <- solve(data, ...)
-    x$setsolve(m)
-    m
+    inv.matrix <- solve(data, ...)
+    x$setsolve(inv.matrix)
+    inv.matrix
 }
+###############################################################
+### Test Script
+# set seed
+set.seed(2) 
+# creating a 4x4 matrix from random normal function
+amatrix<-matrix(rnorm(1:16),nrow=4)  
+# initializing to makeccachematrix fx
+mymatrix<- makecachematrix(amatrix)
+## run twice. 2nd time check to make sure to see message 
+## "getting cached data"
+cacheSolve(mymatrix)
+
